ab7d9e91939fa995fe013ea675a943a007ad1971
Trenton Liebman <trenton.liebman@gmail.com>
2013-11-27 10:56:03 -0800

    commit 3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,13 @@
 
 //= require_tree .
 
-
+$(document).ready(function(){
+	$('#menuToggle, .menu-close').on('click', function(){
+		$('#menuToggle').toggleClass('active');
+		$('body').toggleClass('body-push-toleft');
+		$('#theMenu').toggleClass('menu-open');
+	});
+});
 
  // function nocontext(e) {
 
--- app/assets/javascripts/events.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $('input.timepicker').timepicker({ timeFormat: 'h:mm:p' });
-});
