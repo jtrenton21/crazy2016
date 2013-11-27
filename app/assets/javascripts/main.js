46efa65eff3201d592f04104c07846a2d3e41f5b
Trenton Liebman <trenton.liebman@gmail.com>
2013-11-27 12:30:37 -0800

    new commit

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,11 @@
-$(document).ready(function(){
-	$('#menuToggle, .menu-close').on('click', function(){
-		$('#menuToggle').toggleClass('active');
-		$('body').toggleClass('body-push-toleft');
-		$('#theMenu').toggleClass('menu-open');
-	});
-});
+;(function(){
+
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
